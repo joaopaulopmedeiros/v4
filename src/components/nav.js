2b7b36b0d7541b28ef70e9eefc967a6379a9d15e
João Paulo Pereira de Medeiros <medeiros.p@academico.ifrn.edu.br>
2020-12-16 19:14:49 -0300

    Updating content of static page

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,7 +7,6 @@ import { navLinks } from '@config';
 import { loaderDelay } from '@utils';
 import { useScrollDirection } from '@hooks';
 import { Menu } from '@components';
-import { IconLogo } from '@components/icons';
 
 const StyledHeader = styled.header`
   ${({ theme }) => theme.mixins.flexBetween};
@@ -160,11 +159,11 @@ const Nav = ({ isHome }) => {
               <div className="logo" tabIndex="-1">
                 {isHome ? (
                   <a href="/" aria-label="home">
-                    <IconLogo />
+                    JP
                   </a>
                 ) : (
                   <Link to="/" aria-label="home">
-                    <IconLogo />
+                    JP
                   </Link>
                 )}
               </div>
